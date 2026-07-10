4158ea49a4629e3de66381d1032c46d95f2754ee
AdarshhCodes <adarshsingh79792@gmail.com>
2026-07-10 19:48:11 +0530

    another way to solve LS problem

--- RecursionInJava/LinearSearch.java
+++ RecursionInJava/LinearSearch.java
@@ -1,10 +1,13 @@
 package RecursionInJava;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int arr[] = {1,23,4,56,76,34};
-        int target = 23;
-        System.out.println(find(arr, target, 0));
+        int arr[] = {1,23,4,4,56,76,34};
+        int target = 4;
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(findAllIndex(arr, target, 0, list));
     }
     static int findIndex(int[] arr, int target, int index){
         if(index == arr.length){
@@ -22,5 +25,15 @@ public class LinearSearch {
             return false;
         }
         return arr[index] == target || find(arr, target, index+1);
+    }
+
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer>list){
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index]== target){
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index+1, list);
     }
 }
